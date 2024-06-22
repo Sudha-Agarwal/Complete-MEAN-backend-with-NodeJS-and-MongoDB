caa5e557ecba8de8f50c2a3928bd1a74d126f487
Sudha Agarwal <sudha.agarwal84@gmail.com>
2024-06-22 23:23:20 +0530

    Jasmine

--- MEAN-APP/Frontend/user-app/src/app/signup/signup.component.spec.ts
+++ MEAN-APP/Frontend/user-app/src/app/signup/signup.component.spec.ts
@@ -1,15 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SignupComponent } from './signup.component';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SignupComponent]
-    });
+  beforeEach(async() => {
+    
+    await TestBed.configureTestingModule({
+      declarations: [SignupComponent],
+      imports:[FormsModule, HttpClientTestingModule,]
+    }).compileComponents();
+    //httpMock = TestBed.inject(HttpTestingController);
+
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +25,31 @@ describe('SignupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create a signup Form', ()=>{
+    const formElement: HTMLFormElement = fixture.nativeElement.querySelector('form');
+    const name = formElement.querySelector('input[name="name"]');
+    const age = formElement.querySelector('input[name="age"]');
+
+    //other form elements
+    const submitButton = formElement.querySelector('button[type="submit"]');
+
+    expect(name).toBeTruthy();
+    expect(submitButton).toBeTruthy();
+  });
+
+  it('should call onSubmit method when form is submitted',()=>{
+    spyOn(component, 'onSubmit');
+    const form = fixture.debugElement.query(By.css('form'));
+
+    form.triggerEventHandler('ngSubmit',{});
+    expect(component.onSubmit).toHaveBeenCalled();
+
+  });
+
+  it('should make a POST request/create a new user', ()=>{
+    
+  })
+
+
 });
